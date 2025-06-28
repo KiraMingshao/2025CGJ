d8d69d53ee327bced8c2564c3985fa02fb224fee
u18975442177 <2944917022.com>
2025-06-28 13:12:05 +0800

    ui

--- Assets/Scripts/UI/BattleUI.cs
+++ Assets/Scripts/UI/BattleUI.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BattleUI : MonoBehaviour
 {
-    private Slider hpSlider;
-    private Slider levelSlider;
+    public Slider hpSlider;
+    public Slider levelSlider;
+    public Slider balanceSlider;
 
-    public void Init(int maxHealth)
+    public void Init(int maxHealth,int maxEnergy)
     {
-        
+        hpSlider.maxValue = maxHealth;
+        hpSlider.value = maxHealth;
+        levelSlider.maxValue = 100f;
+        levelSlider.value = 0;
     }
+
+    public void SetBalance(int value)
+    {
+        DOTween.To(() => balanceSlider.value, x => balanceSlider.value = x, value, 0.5f);
+    }
+
+    public void SetHp(int value)
+    {
+        DOTween.To(() => hpSlider.value, x => hpSlider.value = x, value, 0.5f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
