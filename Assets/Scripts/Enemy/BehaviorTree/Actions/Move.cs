8f9e7c3e6d6c032249e0620b176106f00160005e
KiraMingshao <147575671+KiraMingshao@users.noreply.github.com>
2025-06-28 13:38:44 +0800

    1

--- Assets/Scripts/Enemy/BehaviorTree/Actions/Move.cs
+++ Assets/Scripts/Enemy/BehaviorTree/Actions/Move.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 namespace Enemy.BehaviorTree {
     public class MoveAction : Action {
         public float speed;
-        public Rigidbody2D rb;
+        private Rigidbody2D rb;
+
+        public override void OnAwake() {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
         public override TaskStatus OnUpdate() {
             this.rb.velocity = speed * Time.deltaTime * Vector2.left;
