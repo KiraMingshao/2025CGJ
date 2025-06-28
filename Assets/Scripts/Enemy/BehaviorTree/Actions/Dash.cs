2f0c214d08d797a8087c7206f93348370f9968f8
Hell-Tractor <1104840448@qq.com>
2025-06-28 18:14:22 +0800

    replace transform in Dash Action to Vector3

--- Assets/Scripts/Enemy/BehaviorTree/Actions/Dash.cs
+++ Assets/Scripts/Enemy/BehaviorTree/Actions/Dash.cs
@@ -1,12 +1,11 @@
-﻿using BehaviorDesigner.Runtime;
-using BehaviorDesigner.Runtime.Tasks;
+﻿using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine;
 
 namespace Enemy.BehaviorTree {
     public class DashAction : Action {
         public float moveSpeed;
         public float dashSpeed;
-        public SharedTransform startDashPosition;
+        public Vector3 startDashPosition;
         private Enemy enemy;
 
         public override void OnAwake() {
@@ -14,8 +13,8 @@ namespace Enemy.BehaviorTree {
         }
 
         public override TaskStatus OnUpdate() {
-            if (!Mathf.Approximately(Vector3.Distance(this.transform.position, this.startDashPosition.Value.position), 0)) {
-                Vector3 direction = (this.startDashPosition.Value.position - this.transform.position).normalized;
+            if (!Mathf.Approximately(Vector3.Distance(this.transform.position, this.startDashPosition), 0)) {
+                Vector3 direction = (this.startDashPosition - this.transform.position).normalized;
                 this.enemy.rb.velocity = direction * this.moveSpeed * Time.deltaTime * 100;
                 return TaskStatus.Running;
             } else {
