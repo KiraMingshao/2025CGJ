75800c067bb1271c6fca12c74408d3c1a83da4e3
u18975442177 <2944917022.com>
2025-06-28 12:34:14 +0800

    ui

---
+++ Assets/Scripts/UI/BattleUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleUI : MonoBehaviour
+{
+    private Slider hpSlider;
+    private Slider levelSlider;
+
+    public void Init(int maxHealth)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
