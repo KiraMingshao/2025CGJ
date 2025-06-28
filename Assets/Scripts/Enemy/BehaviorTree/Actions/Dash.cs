2f20f6a53dcb7eb04d6ef5fe34a8f2182d61531b
Hell-Tractor <1104840448@qq.com>
2025-06-28 19:37:41 +0800

    fix bug in dash action

--- Assets/Scripts/Enemy/BehaviorTree/Actions/Dash.cs
+++ Assets/Scripts/Enemy/BehaviorTree/Actions/Dash.cs
@@ -9,17 +9,23 @@ namespace Enemy.BehaviorTree {
         public Vector3 endDashPosition;
         public float eps = 0.05f;
         private Enemy enemy;
+        private bool isDashing = false;
 
         public override void OnAwake() {
             this.enemy = this.GetComponent<Enemy>();
         }
 
+        public override void OnStart() {
+            this.isDashing = false;
+        }
+
         public override TaskStatus OnUpdate() {
-            if (Vector3.Distance(this.transform.position, this.startDashPosition) > eps * this.moveSpeed) {
+            if (!this.isDashing && Vector3.Distance(this.transform.position, this.startDashPosition) > eps * this.moveSpeed) {
                 Vector3 direction = (this.startDashPosition - this.transform.position).normalized;
                 this.enemy.rb.velocity = direction * this.moveSpeed * Time.deltaTime * 100;
                 return TaskStatus.Running;
             } else {
+                this.isDashing = true;
                 this.enemy.rb.velocity = this.dashSpeed * 100 * Time.deltaTime * Vector2.left;
                 if (Vector3.Distance(this.transform.position, this.endDashPosition) < eps * this.dashSpeed) {
                     this.enemy.Respawn();
