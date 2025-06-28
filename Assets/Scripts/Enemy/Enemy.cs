0a957fb5ad72e92c6a3280a71c8cb1b2c650c83b
Hell-Tractor <1104840448@qq.com>
2025-06-28 16:33:36 +0800

    add bullet controller and wave controller

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 namespace Enemy {
     public class Enemy : MonoBehaviour {
         public Collider2D attackCollider;
+        public Collider2D bodyCollider;
         public Rigidbody2D rb { get; private set; }
         public Animator animator;
         public GameObject bullet;
         public Transform spawnPosition;
+
+        [Header("Properties")]
+        public int health;
+        public int attack;
+        public int resilience;
+        public int imbalance;
+        public int maxImbalance;
+        [HideInInspector]
+        public int additionalAttack;
 
         [Header("Animator Triggers")]
         public string attackTrigger;
@@ -21,6 +31,7 @@ namespace Enemy {
         public void Attack(int additionalAttack) {
             this.animator.SetTrigger(attackTrigger);
             this.attackCollider.enabled = true;
+            this.additionalAttack = additionalAttack;
         }
 
         public async void Attack(int additionalAttack, float chargeTime) {
@@ -30,13 +41,21 @@ namespace Enemy {
         }
 
         public void Shoot(float bulletSpeed) {
-            var newBullet = Instantiate(bullet, this.transform);
+            var offset = new Vector3(-this.attackCollider.bounds.extents.x / 2, 0f);
+            var newBullet = Instantiate(bullet, this.transform.position + offset, Quaternion.identity);
             newBullet.GetComponent<Rigidbody2D>().velocity = bulletSpeed * 100 * Time.deltaTime * Vector2.left;
+            newBullet.GetComponent<BulletController>().attack = this.attack;
         }
 
         public void Respawn() {
             this.animator.SetTrigger(respawnTrigger);
             this.transform.position = spawnPosition.position;
+        }
+
+        private void Update() {
+            if (this.health <= 0 || this.imbalance >= this.maxImbalance) {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
