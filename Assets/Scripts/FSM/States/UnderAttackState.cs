ef52d4d7f8a787a658c8acbcb9e8c646115d6767
u18975442177 <2944917022.com>
2025-06-29 10:41:19 +0800

    站立和受击动画

--- Assets/Scripts/FSM/States/UnderAttackState.cs
+++ Assets/Scripts/FSM/States/UnderAttackState.cs
@@ -1,4 +1,6 @@
 ﻿using DG.Tweening;
+using UnityEngine;
+
 namespace AI.FSM {
     public class UnderAttackState : FSMState {
         protected override void init() {
@@ -14,6 +16,13 @@ namespace AI.FSM {
         private void PlayUnderAttackAnim(CharacterBattleActionFSM battleActionFSM)
         {
             battleActionFSM.transform.DOPunchPosition(new UnityEngine.Vector3(-10, 0, 0), 0.5f, 5);
+            var sprite = battleActionFSM.GetComponent<SpriteRenderer>();
+            Sequence seq = DOTween.Sequence();
+            //添加动画到序列中
+            seq.Append(DOTween.To(() => sprite.color, x => sprite.color = x, Color.red, 0.2f));
+            seq.Append(DOTween.To(() => sprite.color, x => sprite.color = x, Color.white, 0.3f));
+            Debug.Log("受击");
+            seq.SetAutoKill();
         }
     }
 }
