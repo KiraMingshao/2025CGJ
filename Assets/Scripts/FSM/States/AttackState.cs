ec98b7794404ecd5b54e03ea56b1a4952f5109d6
1993132009 <1993132009@qq.com>
2025-06-29 13:52:30 +0800

    tags

--- Assets/Scripts/FSM/States/AttackState.cs
+++ Assets/Scripts/FSM/States/AttackState.cs
@@ -1,39 +1,56 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace AI.FSM {
-    public class AttackState : FSMState {
+namespace AI.FSM
+{
+    public class AttackState : FSMState
+    {
         private Collider2D attackCollider;
 
-        protected override void init() {
+        protected override void init()
+        {
             this.StateID = FSMStateID.Attack;
         }
 
-        private Collider2D getAttackCollider(CharacterBattleActionFSM fsm) {
+        private Collider2D getAttackCollider(CharacterBattleActionFSM fsm)
+        {
             string lastKey = InputQueue.Instance.LastPressedKey;
-            if (lastKey == "Sweep") {
+            if (lastKey == "Sweep")
+            {
                 return fsm.sweepCollider;
-            } else if (lastKey == "ChopUp") {
+            }
+            else if (lastKey == "ChopUp")
+            {
                 return fsm.chopUpCollider;
-            } else if (lastKey == "ChopDown") {
+            }
+            else if (lastKey == "ChopDown")
+            {
                 return fsm.chopDownCollider;
             }
+
             return null;
         }
 
-        public override void OnStateEnter(FSMBase fsm) {
-            if (fsm is CharacterBattleActionFSM characterFSM) {
+        public override void OnStateEnter(FSMBase fsm)
+        {
+            if (fsm is CharacterBattleActionFSM characterFSM)
+            {
                 this.attackCollider = this.getAttackCollider(characterFSM);
                 this.attackCollider.enabled = true;
 
                 characterFSM.character.AddImbalance(characterFSM.character.attackImbalanceIncrease);
 
-                if (InputQueue.Instance.LastPressedKey == "ChopDown") {
-                    var wave = Object.Instantiate(characterFSM.wave, characterFSM.transform.position, Quaternion.identity);
+                if (InputQueue.Instance.LastPressedKey == "ChopDown")
+                {
+                    var wave = Object.Instantiate(characterFSM.wave, characterFSM.transform.position,
+                        Quaternion.identity);
                     wave.tag = "PlayerWave";
-                    int strength = Mathf.FloorToInt(characterFSM.character.GetDecoratedStatus().attack * characterFSM.waveStrengthFactor * characterFSM.transform.localScale.y);
+                    int strength = Mathf.FloorToInt(characterFSM.character.GetDecoratedStatus().attack *
+                                                    characterFSM.waveStrengthFactor *
+                                                    characterFSM.transform.localScale.y);
                     wave.GetComponent<WaveController>().strength = strength;
-                    wave.GetComponent<Rigidbody2D>().velocity = characterFSM.waveVelocityFactor / strength * Vector2.right;
+                    wave.GetComponent<Rigidbody2D>().velocity =
+                        characterFSM.waveVelocityFactor / strength * Vector2.right;
                     var oldy = wave.transform.localScale.y;
                     wave.transform.localScale = new Vector2(Mathf.Abs(wave.transform.localScale.x) * -1, strength);
                     wave.transform.position = new Vector3(
@@ -45,16 +62,24 @@ namespace AI.FSM {
             }
         }
 
-        public override void OnStateFixedStay(FSMBase fsm) {
-            if (fsm is CharacterBattleActionFSM characterFSM) {
+        public override void OnStateFixedStay(FSMBase fsm)
+        {
+            if (fsm is CharacterBattleActionFSM characterFSM)
+            {
                 List<Collider2D> result = new List<Collider2D>();
-                this.attackCollider.OverlapCollider(new ContactFilter2D() { layerMask = LayerMask.GetMask("Enemy") | LayerMask.GetMask("EnemyBullet") }, result);
-                foreach (var collider in result) {
-                    if (collider.CompareTag("Enemy")) {
+                this.attackCollider.OverlapCollider(
+                    new ContactFilter2D() { layerMask = LayerMask.GetMask("Enemy") | LayerMask.GetMask("EnemyBullet") },
+                    result);
+                foreach (var collider in result)
+                {
+                    if (collider.CompareTag("Enemy"))
+                    {
                         Enemy.Enemy enemy = collider.GetComponent<Enemy.Enemy>();
                         enemy.health -= characterFSM.character.GetDecoratedStatus().attack;
                         enemy.imbalance += characterFSM.character.GetDecoratedStatus().attack - enemy.resilience;
-                    } else {
+                    }
+                    else if (collider.CompareTag("EnemyBullet"))
+                    {
                         BulletController bulletController = collider.GetComponent<BulletController>();
                         bulletController.attack += characterFSM.character.GetDecoratedStatus().attack;
                         Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
@@ -65,8 +90,10 @@ namespace AI.FSM {
             }
         }
 
-        public override void OnStateExit(FSMBase fsm) {
-            if (fsm is CharacterBattleActionFSM characterFSM) {
+        public override void OnStateExit(FSMBase fsm)
+        {
+            if (fsm is CharacterBattleActionFSM characterFSM)
+            {
                 this.attackCollider.enabled = false;
             }
         }
