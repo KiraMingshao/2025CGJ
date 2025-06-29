eb5c291b320577cf5a1f8b5f4305c126a5b93b5a
Hell-Tractor <1104840448@qq.com>
2025-06-29 16:06:01 +0800

    rewrite under attack trigger

--- Assets/Scripts/FSM/Triggers/UnderAttackTrigger.cs
+++ Assets/Scripts/FSM/Triggers/UnderAttackTrigger.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AI.FSM {
     public class UnderAttackTrigger : FSMTrigger {
         public override bool HandleTrigger(FSMBase fsm) {
             if (fsm is CharacterBattleActionFSM battleActionFSM) {
-                return battleActionFSM.bodyCollider.IsTouchingLayers(LayerMask.GetMask("EnemyAttack", "EnemyBullet", "EnemyWave"));
+                List<Collider2D> result = new List<Collider2D>();
+                ContactFilter2D filter = new ContactFilter2D() {
+                    layerMask = LayerMask.GetMask("EnemyAttack", "EnemyBullet", "EnemyWave"),
+                    useTriggers = true,
+                };
+                if (battleActionFSM.bodyCollider.enabled) {
+                    battleActionFSM.bodyCollider.OverlapCollider(filter, result);
+                } else {
+                    battleActionFSM.crouchCollider.OverlapCollider(filter, result);
+                }
+                return result.Count > 0;
             }
             throw new NotSupportedFSMTypeException(this.TriggerID, fsm);
         }
